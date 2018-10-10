0008cd44b0dde3f73d3a135ffeb643d0250adb52
Arkson Costa <arkson@localhost.localdomain>
2018-10-10 14:32:15 -0300

    Configurando locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, LOCALE_ID} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './home/home.component';
+import {registerLocaleData} from '@angular/common';
+import locale from '@angular/common/locales/pt';
+
+registerLocaleData(locale, 'pt');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [{provide: LOCALE_ID, useValue: 'pt'}],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
